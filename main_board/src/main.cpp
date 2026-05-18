0174595ba56b8a0876e726b2079d1ad121b155cf
furtest <paul.retourne@orange.fr>
2026-05-18 18:34:52 +0200

    Temporary main

--- main_board/src/main.cpp
+++ main_board/src/main.cpp
@@ -8,20 +8,40 @@
 #include <Arduino.h>
 #include <usb_serial.h>
 
+#include "bmp.h"
 #include "bno.h"
+#include "core_pins.h"
 #include "debug.h"
 #include "status.h"
+
+#define DEBUG_SERIAL Serial
 
 flight_status_t flight_status;
 
 void setup()
-{	
+{
 	Serial.begin(9600);
 	DEBUG("Program start");
 	init_flight_status(flight_status);
+
+	bno_restore_offsets(flight_status.bno);
+
 	bno_calibrate_sensor(flight_status.bno);
+	//bno_save_offsets(flight_status.bno);
 }
 
 void loop()
 {
+	static int i = 0;
+	bno_data_t bno_data;
+	bno_get_new_events(bno_data, flight_status.bno);
+	bno_print_event(bno_data.orientation);
+	bno_print_event(bno_data.ang_velocity);
+	bno_print_event(bno_data.linear_accel);
+	bno_print_event(bno_data.magnetometer);
+	bno_print_event(bno_data.accelerometer);
+	bno_print_event(bno_data.gravity);
+	bmp_print_data(flight_status);
+	Serial.printf("loop %d\n", i++);
+	delay(1000);
 }
