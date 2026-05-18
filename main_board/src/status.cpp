177a7eb48e48273f610bb66c7d8ca931a39c651b
furtest <paul.retourne@orange.fr>
2026-05-18 18:32:13 +0200

    status: init bno and bmp
    Add the needed call to bno.begin and init the bmp

--- main_board/src/status.cpp
+++ main_board/src/status.cpp
@@ -5,11 +5,20 @@
 
 #include <Adafruit_BMP280.h>
 #include <Adafruit_BNO055.h>
+#include <WireIMXRT.h>
 
+#include "bmp.h"
+#include "debug.h"
 #include "status.h"
 
 void init_flight_status(flight_status_t &status)
 {
 	status.current_stage = PRE_FLIGHT;
-	status.bno = Adafruit_BNO055(55, 0x28);
+	status.bno = Adafruit_BNO055(55, 0x28, &Wire);
+	if(!status.bno.begin()){
+		while (1) {
+			DEBUG("BNO broken");
+		}
+	}
+	bmp_init_sensor(status);
 }
